16ae8a2adcd3986449df3a8d76c73da50b628e1d
Mickaeldolle <dolle.mickael@gmail.com>
2025-06-27 15:21:39 +0200

    :fix remove s to realisationS in viteconfig file

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: './index.html',
-        realisation: './realisations.html',
+        realisation: './realisation.html',
       },
     },
   },
